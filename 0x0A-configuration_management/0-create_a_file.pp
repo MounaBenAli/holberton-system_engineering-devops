4f59ecde67af0a56f0b97d5d4e456a65d076a95b
MounaBenAli <benalimouna1986@gmail.com>
2022-04-15 14:30:53 +0200

    writing my first Puppet manifest(system management tool for server config autotmation )

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Script creates a file in /tmp.
+
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
