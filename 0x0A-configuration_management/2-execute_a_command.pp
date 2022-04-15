7cca22ea6bb4aaf83c2ea7b7f05f55185888ae63
MounaBenAli <benalimouna1986@gmail.com>
2022-04-15 18:03:50 +0200

    fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Manifest kills a process named killmenow using exec resource
 
 exec { 'killmenow':
-command     => '/usr/bin/pkill killmenow',
-onlyif      => '/usr/bin/pgrep killmenow',
+    command     => '/usr/bin/pkill killmenow',
+    onlyif      => '/usr/bin/pgrep killmenow',
 }
